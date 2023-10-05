d6ff706a562d7fedb1e0753fe5b1e6e8ddb0629f
riz-j <adiputra979@gmail.com>
2023-10-05 20:44:20 +1100

    Drizzle ORM works - minor fixes

--- app/api/countries/route.ts
+++ app/api/countries/route.ts
@@ -2,7 +2,7 @@ import { db } from "@/app/db";
 import { countries } from "@/app/db/schema";
 import { NextRequest } from "next/server";
 
-export default async function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
     const countriesList = await db.select().from(countries);
 
     return Response.json(countriesList);
--- app/api/users/route.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "@/app/db";
-import { countries } from "@/app/db/schema";
-import { NextRequest } from "next/server";
-
-export async function GET(request: NextRequest) {
-    const countriesList = await db.select().from(countries);
-
-    return Response.json(countriesList);
-}
