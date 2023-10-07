5adf4649a06c885c68d01583dcd52eed40ea560d
riz-j <adiputra979@gmail.com>
2023-10-07 14:35:26 +1100

    Patch input

--- app/api/countries/[slug]/route.ts
+++ app/api/countries/[slug]/route.ts
@@ -19,15 +19,42 @@ export async function PATCH(
     const countryId = params.slug
     const countryInsert: CountryInsert = await request.json()
 
+    let countryUpdate: any = {}
+
     if (countryInsert.name) { 
-        await db.update(countries).set({ name: countryInsert.name }).where(eq(countries.id, countryId))
+        countryUpdate.name = countryInsert.name
     }
     
     if (countryInsert.longName) {
-        await db.update(countries).set({ longName: countryInsert.longName }).where(eq(countries.id, countryId))
+        countryUpdate.longName = countryInsert.longName
     }
 
+    const validate = validateInput(countryInsert, countryUpdate)
+    if (validate) { return validate }
+
+    await db.update(countries).set(countryUpdate).where(eq(countries.id, countryId))
+
     return new Response(null, { status: 204 })
+}
+
+function validateInput(insertModel: any, updateModel: any) {
+    if (insertModel.id != null) { 
+        return Response.json({ message: "ID must be null" }, { status: 400 }) 
+    }
+
+    for (const prop in insertModel) {
+        if (!(prop in updateModel)) {
+            return Response.json({ message: `Property '${prop}' is not valid` }, { status: 400 })
+        }
+    }
+
+    for (const prop in updateModel) {
+        if (!(prop in insertModel)) {
+            return Response.json({ message: `Property '${prop}' in is not val id` }, { status: 400 });
+        }
+    }
+
+    return null
 }
 
 export async function DELETE(
